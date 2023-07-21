1e00d7a2bfb0d3bc2535414038a84ceb562ae389
olegpl1993 <olegpl1993@gmail.com>
2023-07-21 03:10:27 +0300

    feat: fetch data on server side and render on page

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,9 +1,20 @@
+import RenderStore from "@/components/RenderStore/RenderStore";
 import "./store.scss";
+import { Product } from "@/types";
 
-function Store() {
+async function getData() {
+  const url = "https://msdrop.com.ua/export/4Oyz/json";
+  const res = await fetch(url, { cache: "no-store" });
+  return await res.json();
+}
+
+async function Store() {
+  const data: Product[] = await getData();
+
   return (
     <div className="store">
       <h1 className="store__title">Store page</h1>
+      <RenderStore products={data} />
     </div>
   );
 }
