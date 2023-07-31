fd35d23ef6635f117dc4989b3a196046ba41f0a0
olegpl1993 <olegpl1993@gmail.com>
2023-08-01 02:04:14 +0300

    feat: PriceFilter layout

--- src/components/PriceFilter/PriceFilter.tsx
+++ src/components/PriceFilter/PriceFilter.tsx
@@ -4,7 +4,14 @@ import { useState } from "react";
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 function valuetext(value: number) {
   return `${value}°C`;
@@ -12,9 +19,14 @@ function valuetext(value: number) {
 
 function PriceFilter() {
   const [value, setValue] = useState<number[]>([20, 37]);
-
   const handleChange = (event: Event, newValue: number | number[]) => {
     setValue(newValue as number[]);
+  };
+  const handleSetLowPrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue([Number(event.target.value), value[1]]);
+  };
+  const handleSetHighPrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue([value[0], Number(event.target.value)]);
   };
 
   return (
@@ -26,7 +38,35 @@ function PriceFilter() {
       >
         <Typography className="priceFilter__title">Цена</Typography>
       </AccordionSummary>
-      <AccordionDetails>
+
+      <AccordionDetails className="priceFilter__col">
+        <div className="priceFilter__row">
+          <TextField
+            id="outlined-basic"
+            variant="outlined"
+            size="small"
+            value={value[0]}
+            onChange={handleSetLowPrice}
+            className="priceFilter__input"
+          />
+          -
+          <TextField
+            id="outlined-basic"
+            variant="outlined"
+            size="small"
+            value={value[1]}
+            onChange={handleSetHighPrice}
+            className="priceFilter__input"
+          />
+          <Button
+            variant="outlined"
+            className="priceFilter__button"
+            size="medium"
+          >
+            OK
+          </Button>
+        </div>
+
         <Box className="priceFilter__box">
           <Slider
             getAriaLabel={() => "Temperature range"}
