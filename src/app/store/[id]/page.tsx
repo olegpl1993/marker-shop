d803f587cfb9cf5072751436f3ee692e833764bf
olegpl1993 <olegpl1993@gmail.com>
2023-08-05 22:26:55 +0300

    feat: product page layout

--- src/app/store/[id]/page.tsx
+++ src/app/store/[id]/page.tsx
@@ -2,6 +2,12 @@
 import "./product.scss";
 import Spinner from "@/components/Spinner/Spinner";
 import { useGetProductsQuery } from "@/redux/services/productsApi";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["300", "500"],
+  subsets: ["cyrillic"],
+});
 
 interface Props {
   params: {
@@ -9,7 +15,7 @@ interface Props {
   };
 }
 
-function ProductPage(props: Props) {
+function Product(props: Props) {
   const { id } = props.params;
   const { isLoading, isFetching, data, error } = useGetProductsQuery(null);
   const productData = data?.find((product) => product.sku === id);
@@ -29,23 +35,55 @@ function ProductPage(props: Props) {
   if (productData) {
     return (
       <div className="product">
-        <div className="product__imgBox">
+        <div className="product__galery">
           <img
             className="product__img"
             src={productData.gallery[0]}
             alt={productData.name}
             loading="lazy"
           />
+          <div className="product__imgBox">
+            {productData.gallery.map((img) => (
+              <img
+                key={img}
+                className="product__galeryImg"
+                src={img}
+                alt={productData.name}
+                loading="lazy"
+              />
+            ))}
+          </div>
         </div>
-        <div className="product__descriptionBox">
-          <div>Products sku: {id}</div>
-          <div>Имя: {productData?.name}</div>
-          <div>Цена: {productData?.price}</div>
-          <div>Описание: {productData?.description}</div>
+
+        <div className={`product__descriptionBox ${roboto.className}`}>
+          <div className="product__name">{productData.name}</div>
+          <div className="product__descriptionRow">
+            <div className="product__category">{productData.category}</div>
+            <div className="product__sku">Код: {productData.sku}</div>
+          </div>
+          <div className="product__vendor">Бренд: {productData.vendor}</div>
+          <div className="product__price">Цена: {productData.price}₴</div>
+          <div className="product__color">Цвет: {productData.color}</div>
+          <div className="product__model">Модель: {productData.model}</div>
+          <div className="product__weight">Вес: {productData.weight}</div>
+
+          {productData.sizes && (
+            <div className="product__sizes">
+              {productData.sizes.map((item) => (
+                <div key={item.size} className="product__size">
+                  Размер: {item.size} - {item.amount} шт.
+                </div>
+              ))}
+            </div>
+          )}
+
+          <div className="product__description">
+            Описание: {productData.description}
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default ProductPage;
+export default Product;
