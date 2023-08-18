77d102fe9695261b31a939c965e83716f1c503cb
olegpl1993 <olegpl1993@gmail.com>
2023-08-19 01:23:29 +0300

    feat: sendmail process text

---
+++ src/components/Ellipsis/Ellipsis.tsx
@@ -0,0 +1,13 @@
+import "./Ellipsis.scss";
+
+function Ellipsis() {
+  return (
+    <div className="ellipsis">
+      <div className="ellipsis__dot"></div>
+      <div className="ellipsis__dot"></div>
+      <div className="ellipsis__dot"></div>
+    </div>
+  );
+}
+
+export default Ellipsis;
