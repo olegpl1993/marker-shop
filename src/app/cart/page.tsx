208188da19bc6b65d522e533f557c25bf3db81d9
olegpl1993 <olegpl1993@gmail.com>
2023-08-22 02:41:09 +0300

    fix: cache provider

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -21,25 +21,25 @@ function Cart() {
 
   if (cartProducts.length === 0) {
     return (
-      <CacheProvider value={cache}>
-        <div className="cart">
-          <h1 className="cart__empty">Корзина пустая</h1>
-        </div>
-      </CacheProvider>
+      <div className="cart">
+        <h1 className="cart__empty">Корзина пустая</h1>
+      </div>
     );
   }
 
   return (
-    <div className="cart">
-      <div className="cart__titleRow">
-        <h1 className="cart__title">Корзина</h1>
+    <CacheProvider value={cache}>
+      <div className="cart">
+        <div className="cart__titleRow">
+          <h1 className="cart__title">Корзина</h1>
+        </div>
+        <section className="cart__content">
+          {cartProducts.map((cartProduct) => (
+            <CartCard key={cartProduct.product.sku} cartProduct={cartProduct} />
+          ))}
+        </section>
       </div>
-      <section className="cart__content">
-        {cartProducts.map((cartProduct) => (
-          <CartCard key={cartProduct.product.sku} cartProduct={cartProduct} />
-        ))}
-      </section>
-    </div>
+    </CacheProvider>
   );
 }
 
