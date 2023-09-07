a5bc3ccdd91631a44aa42e9e63b586811f9c3306
Oleg <olegpl1993@gmail.com>
2023-09-08 01:27:01 +0300

    feat: slider navigation

--- src/components/SliderMain/SliderMain.tsx
+++ src/components/SliderMain/SliderMain.tsx
@@ -1,19 +1,22 @@
 "use client";
 import "./SliderMain.scss";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, A11y } from "swiper/modules";
 import { useGetProductsQuery } from "@/redux/services/productsApi";
 import { Product } from "@/types";
+import { Paper } from "@mui/material";
 
 const getUniqueProducts = (data: Product[]) => {
   const uniqueProducts: Product[] = [];
-
   data.forEach((product) => {
     if (!uniqueProducts.find((item) => item.category === product.category)) {
       uniqueProducts.push(product);
     }
   });
-
   return uniqueProducts;
 };
 
@@ -27,23 +30,22 @@ function SliderMain() {
     return (
       <div className="sliderMain">
         <Swiper
+          modules={[Navigation, A11y]}
           className="sliderMain__swiper"
-          spaceBetween={50}
-          slidesPerView={4}
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
-          navigation={true}
+          spaceBetween={60}
+          slidesPerView={5}
+          navigation
         >
           {uniqueProducts.map((product) => (
             <SwiperSlide key={product.sku}>
-              <div className="sliderMain__slide">
+              <Paper elevation={3} className="sliderMain__slide">
                 <img
                   src={product.gallery[0]}
                   alt={product.name}
                   className="sliderMain__img"
                 />
                 <p>{product.category}</p>
-              </div>
+              </Paper>
             </SwiperSlide>
           ))}
         </Swiper>
