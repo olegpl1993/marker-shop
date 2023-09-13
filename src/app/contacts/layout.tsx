adbc0ef1ccab23ff73480ce1e72508add998f94d
Oleg <olegpl1993@gmail.com>
2023-09-13 23:10:25 +0300

    feat: layouts for pages

---
+++ src/app/contacts/layout.tsx
@@ -0,0 +1,21 @@
+import "../global.scss";
+import Footer from "@/components/Footer/Footer";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <div className="container">
+        <main className="main">{children}</main>
+      </div>
+      <div className="footerRow">
+        <div className="container">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
