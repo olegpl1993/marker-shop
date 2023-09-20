01f5836ddbae9de5eeab21b18d715108bd257b1f
Oleg <olegpl1993@gmail.com>
2023-09-21 00:34:05 +0300

    feat: category searchParam

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -12,9 +12,13 @@ import {
   Typography,
 } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { changeSelectedCategories } from "@/redux/slices/selectedCategoriesSlice";
+import {
+  changeSelectedCategories,
+  initializeSelectedCategories,
+} from "@/redux/slices/selectedCategoriesSlice";
 import { changeCurrentPage } from "@/redux/slices/storePaginationSlice";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useEffect } from "react";
 
 const getUniqueCategories = (data: Product[]) => {
   const categories = data.map((product) => product.category);
@@ -24,8 +28,11 @@ const getUniqueCategories = (data: Product[]) => {
 
 function Categories() {
   const { data } = useGetProductsQuery(null);
-
   const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(initializeSelectedCategories());
+  }, []);
+
   const selectedCategories = useAppSelector(
     (state) => state.selectedCategoriesReducer.selectedCategories
   );
@@ -43,33 +50,64 @@ function Categories() {
     dispatch(changeCurrentPage(1));
   };
 
+  //--------------------------------------------------------------
+  // useEffect(() => {
+  //   if (data) {
+  //     const url = new URL(window.location.href);
+  //     const searchParams = new URLSearchParams(url.search);
+  //     const categoriesParam = searchParams.get("categories");
+  //     const categories = getUniqueCategories(data);
+  //     const currentCategories = categories.find(
+  //       (param) => param === searchParams
+  //     );
+  //     if (currentCategories) {
+  //       dispatch(changeSort(currentSort));
+  //     }
+  //   }
+  // }, []);
+
+  // const createQueryString = (name: string, value: string) => {
+  //   const url = new URL(window.location.href);
+  //   const searchParams = new URLSearchParams(url.search);
+  //   const params = new URLSearchParams(searchParams.toString());
+  //   if (value === "") {
+  //     params.delete(name);
+  //   } else {
+  //     params.set(name, value);
+  //   }
+  //   return params.toString();
+  // };
+  //------------------------------------------------------------
+
   const isChecked = (category: string) => selectedCategories.includes(category);
 
   if (data) {
     const categories = getUniqueCategories(data);
     return (
-        <Accordion elevation={3} className="categories">
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography className="categories__title">Категории</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <FormGroup className="categories__list">
-              {categories.map((category) => (
-                <FormControlLabel
-                  control={<Checkbox size="small" checked={isChecked(category)}/>}
-                  className="categories__item"
-                  label={category}
-                  key={category}
-                  onChange={() => handleChangeCategories(category)}
-                />
-              ))}
-            </FormGroup>
-          </AccordionDetails>
-        </Accordion>
+      <Accordion elevation={3} className="categories">
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className="categories__title">Категории</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <FormGroup className="categories__list">
+            {categories.map((category) => (
+              <FormControlLabel
+                control={
+                  <Checkbox size="small" checked={isChecked(category)} />
+                }
+                className="categories__item"
+                label={category}
+                key={category}
+                onChange={() => handleChangeCategories(category)}
+              />
+            ))}
+          </FormGroup>
+        </AccordionDetails>
+      </Accordion>
     );
   }
 }
--- src/redux/slices/selectedCategoriesSlice.ts
+++ src/redux/slices/selectedCategoriesSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { setDataUrl, getDataUrl } from "../dataUrl";
 
 const selectedCategoriesSlice = createSlice({
   name: "selectedCategories",
@@ -6,11 +7,19 @@ const selectedCategoriesSlice = createSlice({
     selectedCategories: [] as String[],
   },
   reducers: {
+    initializeSelectedCategories(state) {
+      const categories = getDataUrl("categories");
+      if (categories) {
+        state.selectedCategories = JSON.parse(categories);
+      }
+    },
     changeSelectedCategories(state, action: PayloadAction<String[]>) {
       state.selectedCategories = action.payload;
+      setDataUrl("categories", JSON.stringify(action.payload));
     },
   },
 });
 
-export const { changeSelectedCategories } = selectedCategoriesSlice.actions;
+export const { changeSelectedCategories, initializeSelectedCategories } =
+  selectedCategoriesSlice.actions;
 export default selectedCategoriesSlice.reducer;
