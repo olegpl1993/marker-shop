2e285d365096bfa03ef51a3379dba18c63c29f38
Oleg <olegpl1993@gmail.com>
2023-09-23 03:02:06 +0300

    feat: price filter to search param

--- src/redux/slices/priceFilterSlice.ts
+++ src/redux/slices/priceFilterSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { getDataUrl, setDataUrl } from "../dataUrl";
 
 const priceFilterSlice = createSlice({
   name: "priceFilter",
@@ -6,11 +7,17 @@ const priceFilterSlice = createSlice({
     priceFilter: [0, 0] as number[],
   },
   reducers: {
+    initializePriceFilter(state) {
+      const initialParams = getDataUrl("priceFilter");
+      if (initialParams) state.priceFilter = JSON.parse(initialParams);
+    },
     changePriceFilter(state, action: PayloadAction<number[]>) {
       state.priceFilter = action.payload;
+      setDataUrl("priceFilter", JSON.stringify(action.payload));
     },
   },
 });
 
-export const { changePriceFilter } = priceFilterSlice.actions;
+export const { changePriceFilter, initializePriceFilter } =
+  priceFilterSlice.actions;
 export default priceFilterSlice.reducer;
