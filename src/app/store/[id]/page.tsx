5b51876ff653ac5f88818c59624c4da3ea9302c2
Oleg <olegpl1993@gmail.com>
2023-09-26 00:14:46 +0300

    feat: breadCrumbs

--- src/app/store/[id]/page.tsx
+++ src/app/store/[id]/page.tsx
@@ -4,6 +4,8 @@ import "./product.scss";
 import Spinner from "@/components/Spinner/Spinner";
 import { useGetProductsQuery } from "@/redux/services/productsApi";
 import ProductDescription from "@/components/ProductDescription/ProductDescription";
+import Link from "next/link";
+import HomeIcon from "@mui/icons-material/Home";
 
 interface Props {
   params: {
@@ -31,8 +33,34 @@ function Product(props: Props) {
   if (productData) {
     return (
       <div className="product">
-        <Gallery productData={productData} />
-        <ProductDescription productData={productData} />
+        <ul className="product__breadCrumbs">
+          <li className="product__breadCrumbsItem">
+            <Link href={"/"} className="product__breadCrumbsLink">
+              <HomeIcon fontSize="small" />
+            </Link>
+            <span className="product__breadCrumbsSeparator">/</span>
+          </li>
+
+          <li className="product__breadCrumbsItem">
+            <Link href={"/store"} className="product__breadCrumbsLink">
+              Магазин
+            </Link>
+            <span className="product__breadCrumbsSeparator">/</span>
+          </li>
+
+          <li className="product__breadCrumbsItem">
+            <Link
+              href={`/store?categories=%5B"${productData.category}"%5D&currentPage=1`}
+              className="product__breadCrumbsLink"
+            >
+              {productData.category}
+            </Link>
+          </li>
+        </ul>
+        <div className="product__section">
+          <Gallery productData={productData} />
+          <ProductDescription productData={productData} />
+        </div>
       </div>
     );
   }
