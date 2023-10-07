b7df4c7a75354939fc9c1390822aeaf066266fd0
Oleg <olegpl1993@gmail.com>
2023-10-08 00:14:03 +0300

    test3

--- src/app/store/[id]/page.tsx
+++ src/app/store/[id]/page.tsx
@@ -1,6 +1,7 @@
 import "./product.scss";
 import ApiService from "@/app/api/apiService";
 import RenderProduct from "@/components/RenderProduct/RenderProduct";
+import { Product } from "@/types";
 
 interface Props {
   params: {
@@ -8,22 +9,28 @@ interface Props {
   };
 }
 
-// export async function generateMetadata(props: Props) {
-//   const { id } = props.params;
-//   const product = await ApiService.getOneProduct(id);
-//   if (product) {
-//     return {
-//       title: product.name,
-//       keywords: product.description,
-//       description: product.description,
-//       openGraph: {
-//         title: product.name,
-//         images: product.gallery[0],
-//         description: product.description,
-//       },
-//     };
-//   }
-// }
+export async function generateMetadata(props: Props) {
+  const { id } = props.params;
+  // const product = await ApiService.getOneProduct(id);
+
+  const url = "https://msdrop.com.ua/export/4Oyz/json";
+  const response = await fetch(url);
+  const data: Product[] = await response.json();
+  const product = data?.find((product) => product.sku === id);
+
+  if (product) {
+    return {
+      title: product.name,
+      keywords: product.description,
+      description: product.description,
+      openGraph: {
+        title: product.name,
+        images: product.gallery[0],
+        description: product.description,
+      },
+    };
+  }
+}
 
 async function Product(props: Props) {
   const { id } = props.params;
